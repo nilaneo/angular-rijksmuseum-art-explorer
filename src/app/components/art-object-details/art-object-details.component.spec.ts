f973c889667b1d1fdc85b696a697c5cac4fe38db
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2018-01-28 09:26:37 +0200

    Refactor unit tests for art details component

--- src/app/components/art-object-details/art-object-details.component.spec.ts
+++ src/app/components/art-object-details/art-object-details.component.spec.ts
@@ -1,24 +1,53 @@
 import { ArtObjectDetailsComponent } from './art-object-details.component';
+import { IArtObjectDetails } from '../../services/rijksmuseum-api/rijksmuseum-api.service';
 
 describe('artObjectDetailsComponent', () => {
   let rijksmuseumApiService: { getDetails: jest.Mock };
   let artObjectDetailsComponent: ArtObjectDetailsComponent;
+  let getDetailsDefer: any;
 
   beforeEach(() => {
     rijksmuseumApiService = {
       getDetails: jest.fn(),
     };
+
+    getDetailsDefer = {};
+    getDetailsDefer.promise = new Promise((resolve, reject) => {
+      getDetailsDefer.resolve = resolve;
+      getDetailsDefer.reject = reject;
+    });
+
+    rijksmuseumApiService.getDetails.mockReturnValue(getDetailsDefer.promise);
+
     artObjectDetailsComponent = new ArtObjectDetailsComponent(rijksmuseumApiService as any);
   });
 
   describe('$onChanges', () => {
+    beforeEach(() => {
+      artObjectDetailsComponent.artObjectDetails = {
+        longTitle: 'Old Art Object',
+        description: 'Some old description',
+        webImage: { url: '...' },
+      };
+      artObjectDetailsComponent.objectNumber = 'abc567';
+    });
+
+    describe('when objectNumber has not been changed', () => {
+      beforeEach(() => {
+        artObjectDetailsComponent.$onChanges({});
+      });
+
+      it('should not unset old details', () => {
+        expect(artObjectDetailsComponent.artObjectDetails).not.toBeUndefined();
+      });
+
+      it('should not load details', () => {
+        expect(rijksmuseumApiService.getDetails).not.toHaveBeenCalled();
+      });
+    });
+
     describe('when objectNumber has been changed', () => {
-      it('should load details', (done) => {
-        const newArtObjectDetails = {
-          longTitle: 'New Art Object',
-          description: 'Some new description',
-          webImage: { url: '...' },
-        };
+      beforeEach(() => {
         const changes = {
           objectNumber: {
             isFirstChange: () => false,
@@ -26,22 +55,34 @@ describe('artObjectDetailsComponent', () => {
             currentValue: 'bac123',
           },
         };
-        artObjectDetailsComponent.artObjectDetails = {
-          longTitle: 'Old Art Object',
-          description: 'Some old description',
-          webImage: { url: '...' },
-        };
         artObjectDetailsComponent.objectNumber = 'bac123';
-        rijksmuseumApiService.getDetails.mockReturnValue(Promise.resolve(newArtObjectDetails));
 
         artObjectDetailsComponent.$onChanges(changes);
+      });
 
+      it('should unset old details', () => {
         expect(artObjectDetailsComponent.artObjectDetails).toBeUndefined();
-        expect(rijksmuseumApiService.getDetails).toHaveBeenCalledWith('bac123');
+      });
 
-        setTimeout(() => {
+      it('should load new details', () => {
+        expect(rijksmuseumApiService.getDetails).toHaveBeenCalledWith('bac123');
+      });
+
+      describe('when details have been loaded', () => {
+        let newArtObjectDetails: IArtObjectDetails;
+
+        beforeEach(() => {
+          newArtObjectDetails = {
+            longTitle: 'New Art Object',
+            description: 'Some new description',
+            webImage: { url: '...' },
+          };
+
+          getDetailsDefer.resolve(newArtObjectDetails);
+        });
+
+        it('should store new details', () => {
           expect(artObjectDetailsComponent.artObjectDetails).toBe(newArtObjectDetails);
-          done();
         });
       });
     });
