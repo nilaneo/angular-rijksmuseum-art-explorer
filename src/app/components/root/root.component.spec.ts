3fda107a6e0f817cf17547799969d8751223f761
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2018-02-04 13:33:02 +0200

    Write the rest of unit tests for root component

--- src/app/components/root/root.component.spec.ts
+++ src/app/components/root/root.component.spec.ts
@@ -8,6 +8,10 @@ describe('rootComponent', () => {
     rootComponent = new RootComponent(SortOrder.ARTIST_ASC);
   });
 
+  it('should save default sort order as current', () => {
+    expect(rootComponent.sortOrder).toBe(SortOrder.ARTIST_ASC);
+  });
+
   describe('$inject', () => {
     it('should return list of dependencies', () => {
       expect(RootComponent.$inject).toEqual([defaultSortOrderToken]);
@@ -65,6 +69,93 @@ describe('rootComponent', () => {
 
     it('should save current page from event', () => {
       expect(rootComponent.pageSize).toBe(100);
+    });
+  });
+
+  describe('onSortOrderChange', () => {
+    describe('when new sort order is undefined', () => {
+      beforeEach(() => {
+        rootComponent.currentPage = 42;
+        rootComponent.sortOrder = SortOrder.RELEVANCE;
+        rootComponent.onSortOrderChange({
+          newSortOrder: undefined,
+        });
+      });
+
+      it('should set sort order to default', () => {
+        expect(rootComponent.sortOrder).toBe(SortOrder.ARTIST_ASC);
+      });
+
+      it('should set current page to 1', () => {
+        expect(rootComponent.currentPage).toBe(1);
+      });
+    });
+
+    describe('when new sort order is not undefined', () => {
+      beforeEach(() => {
+        rootComponent.currentPage = 42;
+        rootComponent.onSortOrderChange({
+          newSortOrder: SortOrder.CHRONOLOGIC,
+        });
+      });
+
+      it('should save sort order from event', () => {
+        expect(rootComponent.sortOrder).toBe(SortOrder.CHRONOLOGIC);
+      });
+
+      it('should set current page to 1', () => {
+        expect(rootComponent.currentPage).toBe(1);
+      });
+    });
+  });
+
+  describe('onListLoad', () => {
+    describe('when count is a multiple of page size', () => {
+      beforeEach(() => {
+        rootComponent.pageSize = 10;
+        rootComponent.onListLoad({
+          artObjectsListResponseData: {
+            artObjects: [],
+            count: 20,
+          },
+        });
+      });
+
+      it('should calculate and save total pages', () => {
+        expect(rootComponent.totalPages).toBe(2);
+      });
+    });
+
+    describe('when last page is not full', () => {
+      beforeEach(() => {
+        rootComponent.pageSize = 10;
+        rootComponent.onListLoad({
+          artObjectsListResponseData: {
+            artObjects: [],
+            count: 24,
+          },
+        });
+      });
+
+      it('should calculate and save total pages', () => {
+        expect(rootComponent.totalPages).toBe(3);
+      });
+    });
+
+    describe('when count is 0', () => {
+      beforeEach(() => {
+        rootComponent.pageSize = 10;
+        rootComponent.onListLoad({
+          artObjectsListResponseData: {
+            artObjects: [],
+            count: 0,
+          },
+        });
+      });
+
+      it('should calculate and save total pages', () => {
+        expect(rootComponent.totalPages).toBe(0);
+      });
     });
   });
 });
