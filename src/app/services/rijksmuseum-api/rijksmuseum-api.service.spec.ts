66650f71818d9c51f67fe9e3e9f149214c21cdfd
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2018-02-04 18:27:23 +0200

    Write unit tests for getDetails of rijksmuseum-api service

--- src/app/services/rijksmuseum-api/rijksmuseum-api.service.spec.ts
+++ src/app/services/rijksmuseum-api/rijksmuseum-api.service.spec.ts
@@ -1,5 +1,10 @@
 import { IPromise } from 'angular';
-import { RijksmuseumApiService, IArtObjectsListResponseData } from './rijksmuseum-api.service';
+import {
+  RijksmuseumApiService,
+  IArtObjectsListResponseData,
+  IArtObjectDetailsResponseData,
+  IArtObjectDetails,
+} from './rijksmuseum-api.service';
 import { SortOrder, defaultSortOrderToken } from '../../values/sort-orders.value';
 
 describe('rijksmuseumApiService', () => {
@@ -31,48 +36,135 @@ describe('rijksmuseumApiService', () => {
   describe('getList', () => {
     let getListResult: IPromise<IArtObjectsListResponseData>;
 
-    beforeEach(() => {
-      getListResult = rijksmuseumApiService.getList({
-        searchQuery: 'some search query',
-        sortOrder: SortOrder.RELEVANCE,
-        page: 23,
-        pageSize: 5,
+    describe('when all params are passed', () => {
+      beforeEach(() => {
+        getListResult = rijksmuseumApiService.getList({
+          searchQuery: 'some search query',
+          sortOrder: SortOrder.RELEVANCE,
+          page: 23,
+          pageSize: 5,
+        });
+      });
+
+      it('should make get request for collection with passed params', () => {
+        expect($http.get).toHaveBeenCalledWith('https://www.rijksmuseum.nl/api/en/collection', {
+          params: {
+            format: 'json',
+            key: '3tYxhQmI',
+            q: 'some search query',
+            s: SortOrder.RELEVANCE,
+            ps: 5,
+            p: 23,
+          },
+        });
+      });
+
+      describe('when get request is done', () => {
+        let artObjectsListResponseData: IArtObjectsListResponseData;
+
+        beforeEach(() => {
+          artObjectsListResponseData = {
+            artObjects: [
+              { objectNumber: 'abc123', title: 'Art #1' },
+              { objectNumber: 'bcd345', title: 'Art #2' },
+            ],
+            count: 2,
+          };
+
+          $httpGetDefer.resolve({
+            data: artObjectsListResponseData,
+          });
+        });
+
+        describe('returned value', () => {
+          it('should be promise resolved by response\'s data', () => {
+            expect(getListResult).resolves.toEqual(artObjectsListResponseData);
+          });
+        });
       });
     });
 
-    it('should make get request for collection with passed params', () => {
-      expect($http.get).toHaveBeenCalledWith('https://www.rijksmuseum.nl/api/en/collection', {
+    describe('when params are not passed', () => {
+      beforeEach(() => {
+        getListResult = rijksmuseumApiService.getList();
+      });
+
+      it('should make get request for collection with passed params', () => {
+        expect($http.get).toHaveBeenCalledWith('https://www.rijksmuseum.nl/api/en/collection', {
+          params: {
+            format: 'json',
+            key: '3tYxhQmI',
+            q: '',
+            s: SortOrder.ARTIST_ASC,
+            ps: 10,
+            p: 1,
+          },
+        });
+      });
+
+      describe('when get request is done', () => {
+        let artObjectsListResponseData: IArtObjectsListResponseData;
+
+        beforeEach(() => {
+          artObjectsListResponseData = {
+            artObjects: [
+              { objectNumber: 'abc123', title: 'Art #1' },
+              { objectNumber: 'bcd345', title: 'Art #2' },
+            ],
+            count: 2,
+          };
+
+          $httpGetDefer.resolve({
+            data: artObjectsListResponseData,
+          });
+        });
+
+        describe('returned value', () => {
+          it('should be promise resolved by response\'s data', () => {
+            return expect(getListResult).resolves.toEqual(artObjectsListResponseData);
+          });
+        });
+      });
+    });
+  });
+
+  describe('getDetails', () => {
+    let getDetailsResult: Promise<IArtObjectDetails>;
+
+    beforeEach(() => {
+      getDetailsResult = rijksmuseumApiService.getDetails('abc123');
+    });
+
+    it('should make get request for details', () => {
+      expect($http.get).toHaveBeenCalledWith('https://www.rijksmuseum.nl/api/en/collection/abc123', {
         params: {
           format: 'json',
           key: '3tYxhQmI',
-          q: 'some search query',
-          s: SortOrder.RELEVANCE,
-          ps: 5,
-          p: 23,
         },
       });
     });
 
-    describe('when get request is done', () => {
-      let artObjectsListResponseData: IArtObjectsListResponseData;
+    describe('when details request is done', () => {
+      let artObjectDetailsResponseData: IArtObjectDetailsResponseData;
 
       beforeEach(() => {
-        artObjectsListResponseData = {
-          artObjects: [
-            { objectNumber: 'abc123', title: 'Art #1' },
-            { objectNumber: 'bcd345', title: 'Art #2' },
-          ],
-          count: 2,
+        artObjectDetailsResponseData = {
+          artObject: {
+            longTitle: 'very long title',
+            description: 'nice art object',
+            webImage: {
+              url: '...',
+            },
+          },
         };
-
         $httpGetDefer.resolve({
-          data: artObjectsListResponseData,
+          data: artObjectDetailsResponseData,
         });
       });
 
       describe('returned value', () => {
         it('should be promise resolved by response\'s data', () => {
-          expect(getListResult).resolves.toEqual(artObjectsListResponseData);
+          return expect(getDetailsResult).resolves.toEqual(artObjectDetailsResponseData.artObject);
         });
       });
     });
