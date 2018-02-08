5a51f60b3c1f23a59c5528229202ece4981287ea
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2018-02-08 09:23:43 +0200

    Migrate AppModule to Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,51 +1,30 @@
-import * as angular from 'angular';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { rijksmuseumApiServiceDeclaration } from './services/rijksmuseum-api/rijksmuseum-api.service';
+import { RijksmuseumApiService } from './services/rijksmuseum-api/rijksmuseum-api.service';
 
-import { sortOrdersValueDeclaration } from './values/sort-orders.value';
+import { RootComponent } from './components/root/root.component';
+import { ArtObjectsListComponent } from './components/art-objects-list/art-objects-list.component';
+import { ArtObjectDetailsComponent } from './components/art-object-details/art-object-details.component';
+import { ArtObjectsSearchComponent } from './components/art-objects-search/art-objects-search.component';
+import { ArtObjectsSortComponent } from './components/art-objects-sort/art-objects-sort.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
-import {
-  rootComponentName,
-  rootComponentOptions,
-} from './components/root/root.component';
-
-import {
-  artObjectsListComponentName,
-  artObjectsListComponentOptions,
-} from './components/art-objects-list/art-objects-list.component';
-
-import {
-  artObjectDetailsComponentName,
-  artObjectDetailsComponentOptions,
-} from './components/art-object-details/art-object-details.component';
-
-import {
-  artObjectsSearchComponentName,
-  artObjectsSearchComponentOptions,
-} from './components/art-objects-search/art-objects-search.component';
-
-import {
-  artObjectsSortComponentName,
-  artObjectsSortComponentOptions,
-} from './components/art-objects-sort/art-objects-sort.component';
-
-import {
-  paginationComponentName,
-  paginationComponentOptions,
-} from './components/pagination/pagination.component';
-
-const appModuleName = 'app';
-const appModule = angular.module(appModuleName, []);
-
-appModule.service(rijksmuseumApiServiceDeclaration);
-
-appModule.value(sortOrdersValueDeclaration);
-
-appModule.component(rootComponentName, rootComponentOptions);
-appModule.component(artObjectsListComponentName, artObjectsListComponentOptions);
-appModule.component(artObjectDetailsComponentName, artObjectDetailsComponentOptions);
-appModule.component(artObjectsSearchComponentName, artObjectsSearchComponentOptions);
-appModule.component(artObjectsSortComponentName, artObjectsSortComponentOptions);
-appModule.component(paginationComponentName, paginationComponentOptions);
-
-export default appModuleName;
+@NgModule({
+  imports: [
+    BrowserModule,
+  ],
+  providers: [
+    RijksmuseumApiService,
+  ],
+  declarations: [
+    RootComponent,
+    ArtObjectsListComponent,
+    ArtObjectDetailsComponent,
+    ArtObjectsSearchComponent,
+    ArtObjectsSortComponent,
+    PaginationComponent,
+  ],
+  bootstrap: [RootComponent],
+})
+export class AppModule {}
