721a53e51d399c1b27d92f86c0e3f8fca8e7b7c9
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2018-02-11 14:38:20 +0200

    Fix problem with sort select

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { RijksmuseumApiService } from './services/rijksmuseum-api/rijksmuseum-api.service';
 import { defaultSortOrderToken, SortOrder } from './values/sort-orders.value';
@@ -16,6 +17,7 @@ import { PaginationComponent } from './components/pagination/pagination.componen
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [
     RijksmuseumApiService,
